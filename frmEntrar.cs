fe2a49b66281ad9d1539a88b920a6139d2340d94
Matheus Alves <matheus.jeje2@gmail.com>
2024-11-29 14:43:33 -0300

    Design da tela de Login

---
+++ frmEntrar.cs
@@ -0,0 +1,25 @@
+namespace Projeto_Agenda
+{
+    public partial class frmEntrar : Form
+    {
+        public frmEntrar()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
